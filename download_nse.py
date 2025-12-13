3fd84f1df31c3081de1ef457666ccec810b10989
Raj11gitHUB <89577651+Raj11gitHUB@users.noreply.github.com>
2025-12-13 12:23:15 +0530

    Update paths for data download functions

--- download_nse.py
+++ download_nse.py
@@ -120,9 +120,10 @@ class Download:
 if __name__ == "__main__":
     d = Download()
     dow_path = "./fiidata"
+    copy_path = "./fiidata"
     start_date = date(2025, 12, 1)
     end_date = date(2025, 12,5)
-    d.bhav_copy(start_date, end_date, dow_path)
-    d.nse_oi(start_date, end_date, dow_path)
-    d.client_oi(start_date, end_date, dow_path, dow_path)
-    d.stats(start_date, end_date, dow_path, dow_path)
+    d.bhav_copy(start_date, end_date, copy_path)
+    d.nse_oi(start_date, end_date, copy_path)
+    d.client_oi(start_date, end_date, dow_path,copy_path)
+    d.stats(start_date, end_date, dow_path, copy_path)
